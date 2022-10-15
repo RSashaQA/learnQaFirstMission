16203a23799a527fb7caaa4dd8c1d51a6f9c777c
RSashaQA <a.romanov.limehd@gmail.com>
2022-10-15 18:15:46 +0300

    Commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MainClass {
+public class MainClass {
+    public int getLocalNumber = 14;
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
+    private String class_string = "Hello, world";
+
+    public String getClassString() {
+        return class_string;
+    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MainClassTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+    @Test
+    public void testGetLocalNumber() {
+        Assert.assertTrue("Attention! " + getLocalNumber + " not equal to 14", getLocalNumber == 14);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Attention! " + getClassNumber() + " not more than 45", getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("Attention! " + getClassString() + " not contains 'hello' or 'Hello'", getClassString().contains("hello") || getClassString().contains("Hello"));
+    }
 }
